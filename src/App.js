dc446fd32c69df85bb3c0472dd46921694e87a72
phichidung <benevolentwarriors@gmail.com>
2019-10-23 18:01:45 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import TodoApp from './Todo';
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <TodoApp/>
+            </div>
+        );
+    }
+}
