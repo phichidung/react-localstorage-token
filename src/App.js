58e3bf0bd9dc322bb7d6eac51a56088091d9d027
phichidung <benevolentwarriors@gmail.com>
2019-10-24 11:05:16 +0700

    remake todo equal localstorage

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ export default class App extends Component {
         },
         () => {
             localStorage.setItem('todo', JSON.stringify(this.state.items));
-            this.setState({ text: "" })
+            this.setState({ text: '' })
         })
     }
 }
